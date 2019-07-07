4f25a42679daf71315d22c547ebc181df046d43f
Alfred <alfredgg@yahoo.es>
2019-07-07 06:43:05 +0200

    Give some info when a mail was not set for the newsletter

--- apps/fok/views/generic.py
+++ apps/fok/views/generic.py
@@ -4,11 +4,15 @@
 from django.views import View
 from ..forms import NewsletterForm
 from django.shortcuts import redirect
+from django.contrib import messages
 
 
 class NewsletterView(View):
     def post(self, request, *args, **kwargs):
         values = request.POST.copy()
+        if 'newsletter' in values.keys() and not values['email']:
+            messages.error(request, 'You accepted to receive the newsletter, however your email address was not given.')
+            return redirect(request.META['HTTP_REFERER'])
         redirect_url = values.pop('redirect_url')[0]
         form = NewsletterForm(instance=request.user, data=values)
         if form.is_valid():
